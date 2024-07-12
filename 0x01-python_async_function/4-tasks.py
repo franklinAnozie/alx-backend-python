949afeac23031fdfa01c63d2d68e6a27864d3462
franklinAnozie <cfanozie@gmail.com>
2024-07-12 15:18:30 +0100

    fixing annotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,17 +2,18 @@
 """ more coroutines """
 
 import asyncio
+from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
         takes an int value and sleeps for a random value of seconds
         between 0 and the value
     """
-    ret_val: list[float] = []
-    n_ret_val: list[float] = []
+    ret_val: List[float] = []
+    n_ret_val: List[float] = []
 
     for num in range(n):
         ret_val.append(task_wait_random(max_delay))
