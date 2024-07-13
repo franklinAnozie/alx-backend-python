8ba3631425504b3848ed3d9e86b77a8fb11303a9
franklinAnozie <cfanozie@gmail.com>
2024-07-13 15:03:01 +0100

    fixed annotation issues

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Async Generator:
         This function returns an async generator
     """
